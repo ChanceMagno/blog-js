b28e61dadeb8544f5e945820b7bccff0b24095be
Chance Magno and Taylor Loftis-Kim <chance@>
2017-04-27 08:44:49 -0700

    Create Successful Delete function for app.

--- app/components/single-article.js
+++ app/components/single-article.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(model) {
+      if (confirm('Are you sure you want to delete this Article?')) {
+        this.sendAction('destroyCar', model);
+      }
+    }
+  }
 });
--- app/routes/single-article.js
+++ app/routes/single-article.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('car', params.car_id);
+  },
+  actions: {
+    destroyCar(model) {
+      model.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
